a01b68861a237b68806744b2f7e98fc5612ad50c
QuilltheMan <innocentsirima@gmail.com>
2023-02-21 06:42:04 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Program to print alphabet letters in all cases followed by new line
+*
+*Return: return 0
+*/
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x02. C - Functions, nested loops
+
+This tasks are on nested loops and fuctions of c
