3befc545f2ddca02ae36d68e3e53914121877ead
QuilltheMan <innocentsirima@gmail.com>
2023-02-28 09:30:04 -0800

    I dont fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,19 +2,15 @@
 
 /**
  * _puts - prints a string
- * @str: string to be printed
- * Return: length of the string
+ * @str: the string
  */
 void _puts(char *str)
 {
-int i = 0;
+	while (*str != '\0')
+	{
+		_putchar(*(str++));
+	}
 	
-while (*(str + i))
-{
-_putchar(*(str + i));
-i++;
+	_putchar('\n');
 }
-_putchar('\n');
-}
-
 
